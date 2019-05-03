2b9d81bce041c4ed54c48e4fb684d17b548452e6
Francisco Aramburu <35516858+FranciscoBuru@users.noreply.github.com>
2019-05-03 00:27:36 -0500

    Update routes.rb

--- proyecto/config/routes.rb
+++ proyecto/config/routes.rb
@@ -9,13 +9,11 @@ Rails.application.routes.draw do
   get '/signin', to: 'pages#signin'
 
   root 'pages#home'
-<<<<<<< HEAD
+
   
   resources :pages
 
   post '/signin', to: 'pages#create'
-=======
 
->>>>>>> 45722cd7f01a3f7d89006a7c3318cb308f3998b5
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
